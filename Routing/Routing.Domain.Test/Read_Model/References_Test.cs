7e773cd126d00f82be0a4423802899266e970e67
Valerio Borioni <valeriob@gmail.com>
2012-03-27 22:10:26 +0200

    working

--- Routing/Routing.Domain.Test/Read_Model/References_Test.cs
+++ Routing/Routing.Domain.Test/Read_Model/References_Test.cs
@@ -6,7 +6,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Routing.Domain.Infrastructure;
 using Autofac;
 using Routing.Domain.ReadModel;
-using Utilities.DynamicSearch;
+//using Utilities.DynamicSearch;
 using Routing.Domain.Dto.Query;
 
 namespace Routing.Domain.Test.Read_Model
@@ -14,27 +14,27 @@ namespace Routing.Domain.Test.Read_Model
     [TestClass]
     public class References_Test
     {
-        [TestMethod]
-        public void Query_Known_Destinations()
-        {
-            var read = Container.Instance.Resolve<References_ReadModel>();
+        //[TestMethod]
+        //public void Query_Known_Destinations()
+        //{
+        //    var read = Container.Instance.Resolve<References_ReadModel>();
 
 
-            var filterA = new ExpressionDataBindableFilter { Operator = FilterOperator.Contains, 
-                PropertyName = "Address", PropertyType=typeof(string), Value = "a" };
-            var filterB = new ExpressionDataBindableFilter { Operator = FilterOperator.Equals, 
-                PropertyName = "Address", PropertyType=typeof(string), Value = "Russi" };
-            var filter = filterA | filterB;
+        //    var filterA = new ExpressionDataBindableFilter { Operator = FilterOperator.Contains, 
+        //        PropertyName = "Address", PropertyType=typeof(string), Value = "a" };
+        //    var filterB = new ExpressionDataBindableFilter { Operator = FilterOperator.Equals, 
+        //        PropertyName = "Address", PropertyType=typeof(string), Value = "Russi" };
+        //    var filter = filterA | filterB;
 
-            var query = new SearchQuery
-            {
-                 PageIndex =0, PageSize = 10, Filter = filter
-            };
+        //    var query = new SearchQuery
+        //    {
+        //         PageIndex =0, PageSize = 10, Filter = filter
+        //    };
 
-            var searchDestinations = new SearchDestinations { Address="russi" };
+        //    var searchDestinations = new SearchDestinations { Address="russi" };
 
-            var result = read.Known_Destinations(searchDestinations);
-        }
+        //    var result = read.Known_Destinations(searchDestinations);
+        //}
 
         [TestMethod]
         public void Search_Scenario()
